1dfae0b2bd5d33280230362429534b4939106511
yuvalelarat <yuvalelarat@gmail.com>
2024-11-23 15:58:32 +0200

    [feat]: delete trip endpoint

--- backend/routes/tripsRoute.js
+++ backend/routes/tripsRoute.js
@@ -6,6 +6,7 @@ import dataSource from "../db/connection.js";
 const router = Router();
 const tripRepository = dataSource.getRepository(Trip);
 
+//create trip
 router.post("/new-trip", async (req, res) => {
   const { trip_name, start_date, end_date, description } = req.body;
 
@@ -58,5 +59,50 @@ router.post("/new-trip", async (req, res) => {
     });
   }
 });
+
+//delete trip
+router.delete("/delete-trip", async (req, res) => {
+  try {
+    const user_id = req.user?.user_id;
+    if (!user_id) {
+      return res.status(401).json({ message: "Unauthorized: Missing user_id" });
+    }
+    console.log(`the user id is ${user_id}`);
+
+    const trip = await tripRepository.findOne({
+      where: {  user: { user_id } },
+      relations: ["user"],
+    });
+
+    if (!trip) {
+      return res.status(404).json({
+        success: false,
+        message: "Trip not found",
+      });
+    }
+
+    if (trip.user.user_id !== user_id) {
+      return res.status(403).json({
+        success: false,
+        message: "You can only delete your own trips",
+      });
+    }
+
+    await tripRepository.delete(trip.trip_id);
+
+    res.status(200).json({
+      success: true,
+      message: "Trip deleted successfully",
+    });
+  } catch (err) {
+    console.error("Error deleting trip:", err);
+    res.status(500).json({
+      success: false,
+      message: "Error deleting trip",
+      error: err.message,
+    });
+  }
+});
+
 
 export default router;
