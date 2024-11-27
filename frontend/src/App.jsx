a427b4584bd4a77a108efa0420d22fb58135b7ca
yuvalelarat <yuvalelarat@gmail.com>
2024-11-27 19:06:50 +0200

    initial commit frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/NavBar/NavBar";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+      <p>TEST</p>
+    </>
+  );
+}
+
+export default App;
