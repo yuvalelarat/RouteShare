3f0a1e44cbbce361da94f8a9b163e1144dcd0ca7
yuvalelarat <yuvalelarat@gmail.com>
2024-12-02 14:57:02 +0200

    added PersistGate to redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store.js';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import App from './App.jsx';
-import store from './redux/store.js';
 import theme from './theme';
 import './index.css';
 
@@ -13,7 +14,9 @@ createRoot(document.getElementById('root')).render(
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <StrictMode>
-                <App />
+                <PersistGate loading={null} persistor={persistor}>
+                    <App />
+                </PersistGate>
             </StrictMode>
         </ThemeProvider>
     </Provider>
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,14 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userDataSlice from './slices/userDataSlice.js';
 import { userDataApi } from './rtk/userDataApi.js';
 
+const persistConfig = {
+    key: 'userData',
+    storage
+};
+
+const persistedUserDataReducer = persistReducer(persistConfig, userDataSlice);
+
 const store = configureStore({
     reducer: {
-        userData: userDataSlice,
+        userData: persistedUserDataReducer,
         [userDataApi.reducerPath]: userDataApi.reducer
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(userDataApi.middleware)
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
