e73b419c715d9e5c40c88d7f4beb43a3e72b47e1
yuvalelarat <yuvalelarat@gmail.com>
2024-12-03 23:45:55 +0200

    tiny fix

--- frontend/src/components/trip/BluredCard/styles.js
+++ frontend/src/components/trip/BluredCard/styles.js
@@ -45,6 +45,7 @@ export const circleStyle = {
     alignItems: 'center',
     marginBottom: '16px',
     boxShadow: 'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px',
+    cursor: 'pointer',
 };
 
 export const headerStyle = {
