a6d74e097706addaa167e12bd24d6b07786dd11e
yuvalelarat <yuvalelarat@gmail.com>
2024-12-06 22:42:42 +0200

    final day activities page headers

--- frontend/src/components/day-activities/DayActivitiesHeaders.jsx
+++ frontend/src/components/day-activities/DayActivitiesHeaders.jsx
@@ -1,8 +1,24 @@
+import Button from '@mui/material/Button';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import './DayActivitiesHeaders.css';
+import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+
 // eslint-disable-next-line react/prop-types
 function DayActivitiesHeaders({ country }) {
+    const { trip_id } = useParams();
+    const navigate = useNavigate();
+    console.log(trip_id);
+    const handleBack = () => {
+        navigate(`/trip/${trip_id}`);
+    };
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <h3 style={{ margin: '0', fontWeight: '400' }}>{` Location: ${country}`}</h3>
+        <div className="day-activities-header">
+            <h3 className="location-header">{` Location: ${country}`}</h3>
+            <Button className="back-button" onClick={handleBack}>
+                <ArrowBackIcon />
+                Back
+            </Button>
         </div>
     );
 }
