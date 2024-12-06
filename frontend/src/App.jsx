ec90302c3611c4bc8b7a3c0d048e32c8677dd3e0
yuvalelarat <yuvalelarat@gmail.com>
2024-12-06 16:57:07 +0200

    added TripRoutes for more privacy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,10 @@ import LoginPage from './pages/LoginPage.jsx';
 import RegisterPage from './pages/RegisterPage.jsx';
 import NewTripPage from './pages/NewTripPage.jsx';
 import MyTripsPage from './pages/MyTripsPage.jsx';
-import UserRoutes from './private-routes/UserRoutes.jsx';
 import TripDaysPage from './pages/TripDaysPage.jsx';
 import ActivitiesDayPage from './pages/ActivitiesDayPage.jsx';
+import UserRoutes from './private-routes/UserRoutes.jsx';
+import TripRoutes from './private-routes/TripRoutes.jsx';
 
 function App() {
     return (
@@ -22,9 +23,11 @@ function App() {
                     <Route element={<UserRoutes />}>
                         <Route path="/my-trips" element={<MyTripsPage />} />
                         <Route path="/new-trip" element={<NewTripPage />} />
-                        <Route path="/trip/:trip_id" element={<TripDaysPage />} />
-                        <Route path="/trip/:trip_id/:journey_id" element={<ActivitiesDayPage />} />
-                        {/*TODO: Make error for random string in url*/}
+                        <Route element={<TripRoutes />}>
+                            <Route path="/trip/:trip_id" element={<TripDaysPage />} />
+                            <Route path="/trip/:trip_id/:journey_id" element={<ActivitiesDayPage />} />
+                            {/*TODO: Make error for random string in url*/}
+                        </Route>
                     </Route>
                 </Routes>
                 {/*TODO: Add footer??*/}
