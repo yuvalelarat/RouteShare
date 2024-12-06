0cc5563a9c24796004a1ec64334ac59d27ae9449
yuvalelarat <yuvalelarat@gmail.com>
2024-12-06 17:33:41 +0200

    fixed enum for activity type

--- backend/models/activity.js
+++ backend/models/activity.js
@@ -28,7 +28,7 @@ export const Activity = new EntitySchema({
     },
     activity_type: {
       type: "enum",
-      enum: ["Sightseeing", "Adventure", "Culture", "Relaxation", "Culinary", "Entertainment"],
+      enum: ["Sightseeing", "Adventure", "Culture", "Relaxation", "Culinary", "Entertainment","Other"],
       nullable: false,
     },
     description: {
