760eddd250688218e34f15e63b29c95347fba124
yuvalelarat <yuvalelarat@gmail.com>
2024-12-18 01:29:52 +0200

    fixing text align to be centered

--- frontend/src/components/common/PageTitle.jsx
+++ frontend/src/components/common/PageTitle.jsx
@@ -1,9 +1,14 @@
 // eslint-disable-next-line react/prop-types
 export default function PageTitle({ title }) {
     return (
-        <h2 style={{ display: 'flex', justifyContent: 'center', marginBottom: 0 }}>
+        <h2
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginBottom: 0,
+                textAlign: 'center',
+            }}>
             {title}
         </h2>
-
     );
 }
--- frontend/src/components/trip-days/TripDetailsTop.jsx
+++ frontend/src/components/trip-days/TripDetailsTop.jsx
@@ -2,15 +2,25 @@
 function TripDetailsTop({ tripAdmin, startDate, endDate, description }) {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <h3 style={{ margin: '0', fontWeight: '400' }}>{`Created by: ${tripAdmin}`}</h3>
-            <h3 style={{ margin: '0', fontWeight: '400' }}>{`${startDate} - ${endDate}`}</h3>
+            <h3
+                style={{
+                    margin: '0',
+                    fontWeight: '400',
+                    textAlign: 'center',
+                }}>{`Created by: ${tripAdmin}`}</h3>
+            <h3
+                style={{
+                    margin: '0',
+                    fontWeight: '400',
+                    textAlign: 'center',
+                }}>{`${startDate} - ${endDate}`}</h3>
             {/*            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '1rem' }}>
                 <Button variant="contained" disableElevation className={'edit-trip-details-button'}>
                     Edit trip details
                 </Button>
             </div>*/}{' '}
             {/*TODO: Implement edit trip details????? it will be bad dates for days cards!!!*/}
-            <p style={{ margin: '0', maxWidth: '80%' }}>{description}</p>
+            <p style={{ margin: '0', maxWidth: '80%', textAlign: 'center' }}>{description}</p>
         </div>
     );
 }
